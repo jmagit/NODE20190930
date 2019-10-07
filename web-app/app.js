b9337c232092063511da9502633ab7da2cf59906
Javier <jmartin@grupoloyal.com>
2019-10-07 18:18:28 +0200

    view

--- web-app/app.js
+++ web-app/app.js
@@ -26,27 +26,48 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.get('/json', (req, res) => {
-  const rslt =  {id: 1, nombre:"Pepito", apellidos:"Grillo"};
+  const rslt = { id: 1, nombre: "Pepito", apellidos: "Grillo" };
 
   res.status(200).json(rslt);
 })
 app.get('/personas', (req, res) => {
   fs.readFile('data/personas.json', 'utf8', (err, data) => {
-    if(err) throw err;
-    const rslt =  {
+    if (err) throw err;
+    const rslt = {
       title: "Mantenimiento de personas",
       listado: JSON.parse(data)
     };
     res.render('persona-list', rslt);
   });
 })
+app.get('/personas/:id', (req, res) => {
+  fs.readFile('data/personas.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    let listado = JSON.parse(data);
+    let rslt = listado.find(item => item.id == req.params.id);
+
+    if (rslt) {
+      const model = {
+        title: "Ver persona",
+        id: rslt.id,
+        nombre: rslt.nombre,
+        apellidos: rslt.apellidos,
+        edad: rslt.edad
+      };
+      res.render('persona-view', rslt);
+    } else {
+      res.status(404).end();
+    }
+  });
+})
+
 app.get('/personas/add', (req, res) => {
-  const rslt =  {title: "Demo formulario", id: 1, nombre:"Pepito", apellidos:"Grillo"};
+  const rslt = { title: "Demo formulario", id: 1, nombre: "Pepito", apellidos: "Grillo" };
 
   res.render('persona-form', rslt);
 })
 app.post('/personas/add', (req, res) => {
-  const rslt =  {
+  const rslt = {
     title: "Demo respuesta del formulario",
     id: 0,
     nombre: req.body.nombre,
@@ -56,12 +77,12 @@ app.post('/personas/add', (req, res) => {
   res.render('persona-view', rslt);
 })
 app.get('/personas/:id/edit', (req, res) => {
-  const model =  {title: "Msntenimiento de personas", id: 1, nombre:"Pepito", apellidos:"Grillo"};
+  const model = { title: "Msntenimiento de personas", id: 1, nombre: "Pepito", apellidos: "Grillo" };
 
   res.render('persona-list', model);
 })
 app.post('/personas/:id/edit', (req, res) => {
-  const rslt =  {
+  const rslt = {
     title: "Demo respuesta del formulario",
     id: 0,
     nombre: req.body.nombre,
@@ -72,19 +93,19 @@ app.post('/personas/:id/edit', (req, res) => {
 })
 
 app.get('/google', (req, res) => {
-  if(!res.headersSent)
+  if (!res.headersSent)
     res.redirect(301, 'https://google.es')
   res.status(500).end();
 })
 app.get('/cotilla/*', (req, res, next) => {
-  res.locals.di='algo';
+  res.locals.di = 'algo';
   res.type('text/plain')
   //res.end();
   next();
 });
 app.get('/cotilla/:id/:cmd', (req, res, next) => {
   let rslt = `ID: ${req.params.id} `;
-  if(req.query.page)
+  if (req.query.page)
     rslt += `page: ${req.query.page} `;
   else {
     res.status(400).end('Falta el page');
@@ -92,9 +113,9 @@ app.get('/cotilla/:id/:cmd', (req, res, next) => {
     //next(new Error('Falta el page'));
     return;
   }
-  if(req.query.size) rslt += `size: ${req.query.size} `;
-  if(req.get('Accept-Language')) rslt += `idioma: ${req.get('Accept-Language')} `;
-  if(req.body) {
+  if (req.query.size) rslt += `size: ${req.query.size} `;
+  if (req.get('Accept-Language')) rslt += `idioma: ${req.get('Accept-Language')} `;
+  if (req.body) {
     rslt += `nombre: ${req.body.nombre} `;
     rslt += `apellidos: ${req.body.apellidos} `;
   }
@@ -103,7 +124,7 @@ app.get('/cotilla/:id/:cmd', (req, res, next) => {
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404).end();
   //next(createError(404));
   next();
@@ -112,7 +133,7 @@ app.use(function(req, res, next) {
 
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
